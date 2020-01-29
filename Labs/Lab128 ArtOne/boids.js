96c9a7485b3497979da735410ddfe8acd4214eac
798308 <54591150+798308@users.noreply.github.com>
2020-01-29 08:50:38 -0800

    Me and the boids

--- Labs/Lab128 ArtOne/boids.js
+++ Labs/Lab128 ArtOne/boids.js
@@ -1,20 +1,43 @@
 // Austin Matel
 //1/28/20
-class Boids{
-  constructor(x,y,dx,dy){
-    this.loc = createVector(x,y);
-    this.vel = createVector(dx,dy);
+class Boid{
+  constructor(){
+    this.loc = createVector(random(0,800), random(0,800));
+    this.vel = createVector(-8,8);
+    this.acc = createVector(0,0);
+    this.clr = color(random(0,255),random(0,255),random(0,255));
   }
   run(){
-
+    this.render();
+    this.update();
+    this.checkEdges();
   }
   render(){
-
+    for(var i = 0; i < boids.length; i++){
+      var disToBoids = this.loc.dist(boids[i]);
+      if (disToBoids < 100){
+        fill(this.clr);
+        line(this.loc.x, this.loc.y, boids[i].loc.x, boids[i].loc.y);
+        console.log("ok");
+      }
+    }
   }
   update(){
-
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
   }
   checkEdges(){
-    
+    if(this.loc.x > 800){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y > 800){
+      this.vel.y = -this.vel.y;
+    }
+    if(this.loc.y < 0){
+      this.vel.y = -this.vel.y;
+    }
+    if(this.loc.y > 800){
+      this.vel.y = -this.vel.y;
+    }
   }
 }
--- Labs/Lab128 ArtOne/sketch.js
+++ Labs/Lab128 ArtOne/sketch.js
@@ -3,16 +3,25 @@
 //  This is a comment
 //  The setup function function is called once when your program begins
 var boids = [];
-function setup() {
+var boidNum = 10;
+function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
-  fill(200, 30, 150);
-
-
+  loadBoids(boidNum);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-
+  runBoids();
+}
+function loadBoids(){
+  for(var i = 0; i < boidNum; i++){
+    boids[i] = new Boid();
+  }
+}
+function runBoids(){
+  for(var i = 0; i < boids.length; i++){
+    boids[i].run();
+  }
 }
