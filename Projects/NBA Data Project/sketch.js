5bae6bac22d5479ae1c0557c518d2455baa7bcad
798308 <54591150+798308@users.noreply.github.com>
2020-02-05 08:47:44 -0800

    Data Project Start

---
+++ Projects/NBA Data Project/sketch.js
@@ -0,0 +1,18 @@
+//  Austin Matel
+// 	02/05/20
+//  stats are here: https://www.kaggle.com/drgilermo/nba-players-stats#Seasons_Stats.csv
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
