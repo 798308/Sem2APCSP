b9403cbd3cafc509b2777c586f534b3d70699004
798308 <54591150+798308@users.noreply.github.com>
2020-02-07 08:44:52 -0800

    Im getting somewhere for once in my life

--- Projects/NBA Data Project/sketch.js
+++ Projects/NBA Data Project/sketch.js
@@ -3,20 +3,23 @@
 //  stats are here: https://www.kaggle.com/drgilermo/nba-players-stats#Seasons_Stats.csv
 //  This is a comment
 //  The setup function function is called once when your program begins
-statsArray = [];
+var statsArray =[];
+var stats;
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  //Im confused
-loadPlayerStats("Stephen Curry");
-y = map(values[i], smallest, largest, 0, drawAreaHeight);
+  loadStats();
+  loadPlayerStats("Michael Jordan");
+  aggregateStats("Michael Jordan", 10);
+  console.log(statsArray[10]);
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-
+  createPlayerSelectionList();
+  getSelectedPlayers();
 }
 function loadPlayerStats(player){
   statsArray = stats.findRows(player,2);
@@ -31,11 +34,11 @@ function aggregateStats(player, stat){
   }
   return results;
 }
-function createPlayerSelectionList() {
+function createPlayerSelectionList(){
   playerSel = createSelect(true);
   playerSel.position((windowWidth-width)/2 + 270, (windowHeight-height)/2 + 40);
 }
-function getSelectedPlayers() {
+function getSelectedPlayers(){
   chosenPlayers = [];
   for(var i = 0; i < playerSel.elt.selectedOptions.length; i++) {
     chosenPlayers.push(playerSel.elt.selectedOptions[i].value);
